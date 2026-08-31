206c76d06bd8babad7a1d586ab0d0d2b19866465
Federico Pérez Errisuriz <federico.pereze.pe@gmail.com>
2026-08-30 22:25:20 -0600

    fix: Empleados model.rs

--- Backend/src/Empleados/Empleados/model/model.rs
+++ Backend/src/Empleados/Empleados/model/model.rs
@@ -1,17 +1,19 @@
+pub struct empleados{
+    id_emplado: i64,
+    id_usuario: i64,
+    nombre: String,
+    apellido_paterno: result<String>,
+    apellido_materno: result<String>,
+    telefono: result<String>,
+    rfc: String,
+    fecha_ingreso: date::NaiveDate,
+    foto_perfil: result<String>,
+    created_at: date::NaiveDateTime,
+    is_active: bool,
+    id_rol: i64,
+    id_plantaDestino: i64,
+}
+
 impl model{
-    pub struct empleados{
-        id_emplado: i64,
-        id_usuario: i64,
-        nombre: String,
-        apellido_paterno: result<String>,
-        apellido_materno: result<String>,
-        telefono: result<String>,
-        rfc: String,
-        fecha_ingreso: date::NaiveDate,
-        foto_perfil: result<String>,
-        created_at: date::NaiveDateTime,
-        is_active: bool,
-        id_rol: i64,
-        id_plantaDestino: i64,
-    }
+    
 }
